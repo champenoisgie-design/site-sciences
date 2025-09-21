33be81d85ffe5464367d3d61bc962303267a9b94
champenoisgie-design <champenois.gie@gmail.com>
2025-09-21 10:06:08 +0200

    chore(postcss): config v4 (@tailwindcss/postcss + autoprefixer) + import Tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 // postcss.config.mjs
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
 export default {
   plugins: [tailwindcss, autoprefixer],
+=======
+/** @type {import('postcss-load-config').Config} */
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
+>>>>>>> 47c9ff3 (chore(postcss): config v4 (@tailwindcss/postcss + autoprefixer) + import Tailwind)
 };
